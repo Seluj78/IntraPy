b0f75c21cac05c959f1f77466a177c7ae27a74ca
Jules Lasne (jlasne) <jlasne@student.42.fr>
2017-12-22 20:33:12 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'IntraPy',
   packages = ['IntraPy'],
-  version = '0.1.0',
+  version = '0.1.1',
   description = 'A Python Library to use easily the 42 API',
   author = 'Jules Lasne',
   author_email = 'jlasne@student.42.fr',
