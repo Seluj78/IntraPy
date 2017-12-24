2d60e0e8f6a1ba889ebc7cfdf39882e26d8eb48e
Jules Lasne (jlasne) <jlasne@student.42.fr>
2017-12-24 09:13:24 +0100

    [FIX] Fixed #5 where the coalition ids would change based on score

--- IntraPy/coalition_handler.py
+++ IntraPy/coalition_handler.py
@@ -20,28 +20,6 @@ import json
 from typing import List
 from IntraPy import IntraPy
 
-COALITIONS = [
-    (0, 'FEDERATION', 'the-federation', 'The Federation'),
-    (1, 'ALLIANCE', 'the-alliance', 'The Alliance'),
-    (2, 'ASSEMBLY', 'the-assembly', 'The Assembly'),
-    (3, 'ORDER', 'the-order', 'The Order')
-]
-
-
-def coalition_get_score(app_token: str, coalition: int) -> int:
-    response = IntraPy.api_get(app_token, "/v2/blocs/1", "GET")
-    ret = json.loads(response.content)
-    return ret['coalitions'][coalition]['score']
-
-
-def coalitions_get_scores(app_token: str) -> List[int]:
-    scores = []
-    response = IntraPy.api_get(app_token, "/v2/blocs/1", "GET")
-    ret = json.loads(response.content)
-    for coa in COALITIONS:
-        scores.append(ret['coalitions'][coa[0]]['score'])
-    return scores
-
 
 def coalition_get_name(app_token: str, coalition: int) -> str:
     response = IntraPy.api_get(app_token, "/v2/blocs/1", "GET")
@@ -53,8 +31,10 @@ def coalitions_get_names(app_token: str) -> List[str]:
     names = []
     response = IntraPy.api_get(app_token, "/v2/blocs/1", "GET")
     ret = json.loads(response.content)
-    for coa in COALITIONS:
-        names.append(ret['coalitions'][coa[0]]['name'])
+    coalition_id = 0
+    while coalition_id <= 3:
+        names.append(ret['coalitions'][coalition_id]['name'])
+        coalition_id += 1
     return names
 
 
@@ -68,6 +48,25 @@ def coalitions_get_slugs(app_token: str) -> List[int]:
     slugs = []
     response = IntraPy.api_get(app_token, "/v2/blocs/1", "GET")
     ret = json.loads(response.content)
-    for coa in COALITIONS:
-        slugs.append(ret['coalitions'][coa[0]]['slug'])
+    coalition_id = 0
+    while coalition_id <= 3:
+        slugs.append(ret['coalitions'][coalition_id]['slug'])
+        coalition_id += 1
     return slugs
+
+
+def coalition_get_score(app_token: str, coalition: int) -> int:
+    response = IntraPy.api_get(app_token, "/v2/blocs/1", "GET")
+    ret = json.loads(response.content)
+    return ret['coalitions'][coalition]['score']
+
+
+def coalitions_get_scores(app_token: str) -> List[int]:
+    scores = []
+    response = IntraPy.api_get(app_token, "/v2/blocs/1", "GET")
+    ret = json.loads(response.content)
+    coalition_id = 0
+    while coalition_id <= 3:
+        scores.append(ret['coalitions'][coalition_id]['score'])
+        coalition_id += 1
+    return scores
