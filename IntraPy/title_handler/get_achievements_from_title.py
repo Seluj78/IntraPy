b97667bc103d0772db62a5d7f04b41686aee4920
Jules Lasne (jlasne) <jlasne@student.42.fr>
2017-12-26 14:57:30 +0100

    [FEATURE] Added possibility to get achievements form title

---
+++ IntraPy/title_handler/get_achievements_from_title.py
@@ -0,0 +1,27 @@
+"""
+    A python Library to help make python apps/bots using the 42 API
+    Copyright (C) 2017-2018  Jules LASNE jlasne@student.42.fr
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import json
+from IntraPy import IntraPy
+
+
+def get_achievements_from_title(app_token: str, title_id: int):
+    response = IntraPy.api_get(app_token, "/v2/titles/" +
+                               str(title_id) + "/achievements", "GET")
+    ret = json.loads(response.content)
+    return ret
