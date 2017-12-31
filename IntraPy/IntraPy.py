96cec683557f2b37760deec23cbafb744a0ac055
Jules Lasne (jlasne) <jlasne@student.42.fr>
2017-12-31 17:36:12 +0100

    [DOC] Documented IntraPy.py

--- IntraPy/IntraPy.py
+++ IntraPy/IntraPy.py
@@ -24,7 +24,7 @@ from IntraPy.config import APP_UID, APP_SECRET, TOKEN_FILE
 
 class IntraPy:
     """
-    This will be the main class for IntraPy
+        This is the main class for IntraPy
     """
 
     def __init__(self):
@@ -40,6 +40,11 @@ class IntraPy:
         self.app_token = IntraPy.check_app_token(self)
 
     def api_request_new_token(self):
+        """
+        This function will request a new token to the 42 api
+
+        :return: Returns the newly requested token
+        """
         d = {'grant_type': 'client_credentials',
              'client_id': self.app_uid, 'client_secret': self.app_secret}
         r = requests.post("https://api.intra.42.fr/oauth/token", data=d)
@@ -50,6 +55,12 @@ class IntraPy:
         return r.json()['access_token']
 
     def test_token(self):
+        """
+        This function will test on /v2/accreditations if the token is still
+        available and hasn't expired
+
+        :return: Returns True if the token is still usable, false otherwise
+        """
         self.app_token = IntraPy.get_token_from_file(self)
         h = {'Authorization': 'Bearer ' + self.app_token}
         r = requests.request("GET",
@@ -64,10 +75,23 @@ class IntraPy:
         return True
 
     def get_token_from_file(self):
+        """
+        :return: Returns the first line of the token file containing the
+        app token.
+        """
         with open(self.token_file, 'r+') as file:
             return file.readline()
 
     def check_app_token(self):
+        """
+        This function will check every possible case of error possible with
+        the app token:
+        If the file doesn't exists
+        If the file is empty
+        If the token is expired/incomplete
+
+        :return: Returns the app_token once every check is done
+        """
         if os.path.exists(TOKEN_FILE):
             if os.stat(TOKEN_FILE).st_size != 0:
                 if IntraPy.test_token(self):
@@ -84,6 +108,15 @@ class IntraPy:
         return self.app_token
 
     def api_get(self, uri: str, methods="GET"):
+        """
+
+        This function will handle all the API requests.
+
+        :param uri: The url you want to request from
+        :param methods: The method you want to to your API request on. By default, `methods` is set to GET
+
+        :return: Returns the response object returned by requests.request
+        """
         h = {'Authorization': 'Bearer ' + self.app_token}
         r = requests.request(methods, "https://api.intra.42.fr" +
                              uri, headers=h, allow_redirects=False)
