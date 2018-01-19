46dcb55ab8c8dea495a09caa003cfac7f6310b5b
Jules Lasne (jlasne) <jlasne@student.42.fr>
2018-01-19 14:05:03 +0100

    [FEATURE] Started adding test to try code coverage

--- tests/test_IntraPy.py
+++ tests/test_IntraPy.py
@@ -1,29 +1,42 @@
-from unittest import TestCase
+"""
+    A python Library to help make python apps/bots using the 42 API
+    Copyright (C) 2017-2018  Jules LASNE jlasne@student.42.fr
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from nose.tools import assert_is_not_none
 from unittest.mock import Mock, patch
-from nose.tools import assert_list_equal
-from nose.tools import assert_is_none
 from IntraPy.IntraPy import IntraPy
 
 
-class TestIntraPy(TestCase):
-    @patch('IntraPy.IntraPy.requests.request')
-    def test_api_request_new_token(self, mock_get):
-        fake_return = [{
-            'access_token': 'df9580fc7b3c3501cb39646a89050c774ee3a43b3e55633345ef931ffecdf6e6',
-            'token_type': 'bearer',
-            'expires_in': 7200,
-            'scope': 'public',
-            'created_at': 1514886496
-        }]
-        mock_get.return_value = Mock(ok=True)
-        mock_get.return_value.json.return_value = fake_return
-        intra = IntraPy()
-        response = intra.api_request_new_token()
-        assert_list_equal(response, fake_return[0]['access_token'])
+@patch('IntraPy.IntraPy.requests.request')
+def test_api_with_good_url(mock_get):
+    # Configure the mock to return a response with an OK status code.
+    mock_get.return_value.ok = True
+    # Call the service, which will send a request to the server.
+    api = IntraPy()
+    response = api.api_get_single("/v2/achievements")
+    # If the request is sent successfully, then I expect a response to be returned.
+    assert_is_not_none(response)
 
-    @patch('IntraPy.IntraPy.requests.request')
-    def test_api_request_new_token_fail(self, mock_get):
-        mock_get.return_value.ok = False
-        intra = IntraPy()
-        response = intra.api_request_new_token()
-        assert_is_none(response)
+
+@patch('IntraPy.IntraPy.requests.request')
+def test_api_with_bad_url(mock_get):
+    # Configure the mock to return a response with an OK status code.
+    mock_get.return_value.ok = False
+    # Call the service, which will send a request to the server.
+    api = IntraPy()
+    response = api.api_get_single("/v2/thisdoesntexist")
+    # If the request is sent successfully, then I expect a response to be returned.
+    assert_is_not_none(response)
