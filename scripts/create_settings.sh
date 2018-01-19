7a6f5a1f053dda99d6ba19e7e801f52925b02398
Jules Lasne (jlasne) <jlasne@student.42.fr>
2018-01-19 14:19:47 +0100

    [WOW] Such tired, much wow

--- scripts/create_settings.sh
+++ scripts/create_settings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat "[settings]" > settings.ini
-cat "APP_UID=$APP_UID" >> settings.ini
-cat "APP_SECRET=$APP_SECRET" >> settings.ini
-cat "TOKEN_FILE=$TOKEN_FILE" >> settings.ini
+echo "[settings]" > settings.ini
+echo "APP_UID=$APP_UID" >> settings.ini
+echo "APP_SECRET=$APP_SECRET" >> settings.ini
+echo "TOKEN_FILE=$TOKEN_FILE" >> settings.ini
