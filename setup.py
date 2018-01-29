706e9e4c7e63a49b6a1593a5394cad5d27b6efd7
Jules Lasne (jlasne) <jlasne@student.42.fr>
2018-01-29 12:56:41 +0100

    [CHORE] version number problems
    Crazy how a commit can mess things up

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name='IntraPy',
   packages=['IntraPy'],
-  version='0.1.7.1',
+  version='0.2.1',
   description='A Python Library to use easily the 42 API',
   author='Jules Lasne',
   author_email='jlasne@student.42.fr',
   url='https://github.com/seluj78/IntraPy',
-  download_url='https://github.com/seluj78/IntraPy/archive/0.1.7.1.tar.gz',
+  download_url='https://github.com/seluj78/IntraPy/archive/0.2.1.tar.gz',
   keywords=['42', 'API', 'python'],
   install_requires=[
     'json',
